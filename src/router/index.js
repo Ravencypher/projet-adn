00132564e7b76a6aca025aeedc27d754fe05085c
Stephaniedenis <93406932+Stephaniedenis@users.noreply.github.com>
2022-11-14 20:26:28 -0500

    création de la page Boycott

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import {
   import CreationBoycott from"../views/CreationBoycott.vue";
   import ProfilUtilisateur from"../views/ProfilUtilisateur.vue";
   import RechercheMembre from"../views/RechercheMembre.vue";
+  import PageBoycott from"../views/PageBoycott.vue";
 /* import NotFound from '../views/404.vue'; */
   
   const router = createRouter({
@@ -45,7 +46,12 @@ import {
         path:"/rechercheMembre",
         name:"RechercheMembre",
         component: RechercheMembre,
-      }
+      },
+      {
+        path:"/pageBoycott",
+        name:"PageBoycott",
+        component: PageBoycott,
+      },
 /*       {
         path: "/inscription,
         name: "Inscription",
