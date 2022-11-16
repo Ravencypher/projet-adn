399e30b2c50ab8eed0536b224159a949a1877c4e
Ravencypher <ravencypher@outlook.com>
2022-11-16 16:40:02 -0500

    Mise en page connexion, inscription et recherche

--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -1,20 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <HeaderNav />
-    <main class="body-container-onepage">
-      <form method="post" action="" class="p-5 d-flex flex-column">
-        <h2 class="text-center">Connexion</h2>
-        <label for="pseudo">Pseudonyme</label>
-      <input type="text" id="pseudo" name="pseudo" required="required">
-            <label for="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required="required">
-            <button type="submit" class="mx-auto mt-4 p-2 d-flex align-items-center justify-content-center">Connexion</button>
-            <p>Pas de compte? <span><router-link to="/inscription">Connexion</router-link></span></p>
-        </form>
-
-
-    </main>
-    <FooterCo />
+  <HeaderNav />
+  <main class="body-container-onepage">
+    <form method="post" action="" class="p-5 d-flex flex-column">
+      <h2 class="text-center">Connexion</h2>
+      <label for="pseudo">Pseudonyme</label>
+      <input type="text" id="pseudo" name="pseudo" required="required" />
+      <label for="password">Mot de passe</label>
+      <input type="password" id="password" name="password" required="required" />
+      <button
+        type="submit"
+        class="mx-auto mt-4 p-2 d-flex align-items-center justify-content-center"
+      >
+        Connexion
+      </button>
+      <p class="text-center mt-4">
+        Pas de compte?
+        <span><router-link to="/inscription">Connexion</router-link></span>
+      </p>
+    </form>
+  </main>
+  <FooterCo />
 </template>
 
 <script>
@@ -25,6 +31,6 @@ export default {
   components: {
     HeaderNav,
     FooterCo,
-  }
-}
+  },
+};
 </script>
--- src/views/RechercheMembre.vue
+++ src/views/RechercheMembre.vue
@@ -4,27 +4,27 @@
     <form method="post" action="" class="p-5 d-flex flex-column">
       <h2 class="text-center">Rechercher un utilisateur</h2>
       <label for="ville">Ville</label>
-      <input type="text" id="ville" name="ville">
-      <p>ou</p>
+      <input type="text" id="ville" name="ville" />
+      <span class="mx-auto or">ou</span>
       <label for="pays">Pays</label>
-      <input type="text" id="pays" name="pays">
-            <button type="submit" class="mx-auto mt-4 p-2 d-flex align-items-center justify-content-center">Rechercher</button>
-            <router-link to="/inscription"></router-link>
-        </form>
-
-
-    </main>
-   <FooterCo />
+      <input type="text" id="pays" name="pays" />
+      <button type="submit" class="mx-auto mt-4 p-2 d-flex align-items-center justify-content-center">
+        Rechercher
+      </button>
+      <router-link to="/inscription"></router-link>
+    </form>
+  </main>
+  <FooterCo />
 </template>
 <script>
 import HeaderNav from "../components/HeaderNav.vue";
 import FooterCo from "../components/FooterCo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderNav,
     FooterCo,
-  }
-}
+  },
+};
 </script>
